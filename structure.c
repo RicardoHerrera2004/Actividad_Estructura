31d8422dbebb8959b2020118058cdc35636c9392
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2024-01-16 22:31:49 -0500

    Primer commit: Estructura

---
+++ structure.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <stdlib.h>
+//Se incluyen las bibliotecas
+
+struct alumno { 
+    int matricula; 
+    char nombre[50]; 
+    char direccion[50]; 
+    char carrera[50]; 
+    float promedio; 
+};
+//Se crea la estructura para alumno en general
+
+
